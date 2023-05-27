e4e761177d9d2d1f09af3dabbbed3b06d6c915d0
abhi-navz <abhinavg01441@gmail.com>
2023-05-27 08:20:31 +0530

    added one more section

--- Paytm Homepage clone/script.js
+++ Paytm Homepage clone/script.js
@@ -1,28 +1,31 @@
-const scrollContainer = document.querySelector('.paytmMoneyInstruments');
+const scrollContainer = document.querySelector(".paytmMoneyInstruments");
 
 const scrollLeft = () => {
   scrollContainer.scrollBy({
     left: -200,
-    behavior: 'smooth'
+    behavior: "smooth",
   });
 };
 
 const scrollRight = () => {
   scrollContainer.scrollBy({
     left: 200,
-    behavior: 'smooth'
+    behavior: "smooth",
   });
 };
 
 const leftButton = document.getElementById("leftScroll");
-leftButton.classList.add('scroll-button');
+leftButton.classList.add("scroll-button");
 
-leftButton.addEventListener('click', scrollLeft);
+leftButton.addEventListener("click", scrollLeft);
 
-const rightButton = document.getElementById("rightScroll")
+const rightButton = document.getElementById("rightScroll");
 
-rightButton.classList.add('scroll-button');
-rightButton.addEventListener('click', scrollRight);
+rightButton.classList.add("scroll-button");
+rightButton.addEventListener("click", scrollRight);
 
 scrollContainer.parentNode.insertBefore(leftButton, scrollContainer);
-scrollContainer.parentNode.insertBefore(rightButton, scrollContainer.nextSibling);
+scrollContainer.parentNode.insertBefore(
+  rightButton,
+  scrollContainer.nextSibling
+);
