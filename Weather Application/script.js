12b550ed5a908ad3feede222d7fd92221c539671
abhi-navz <abhinavg01441@gmail.com>
2023-07-03 12:18:24 +0530

    added more details

--- Weather Application/script.js
+++ Weather Application/script.js
@@ -13,6 +13,7 @@ function getWeather() {
   fetch(`${weatherEndpoint}?q=${location}&appid=${apiKey}&units=metric`)
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
       const weatherInfo = document.getElementById("weatherInfo");
 
       // Clear previous weather info
@@ -22,18 +23,40 @@ function getWeather() {
         weatherInfo.innerHTML = `<p>No weather information found for "${data.message}".</p>`;
       } else {
         const locationName = data.name;
+        const countryName = data.sys.country;
+        const longitude = data.coord.lon;
+        const latitude = data.coord.lat;
+        const weatherMain = data.weather[0].main;
         const description = data.weather[0].description;
         const temperature = data.main.temp;
         const humidity = data.main.humidity;
         const windSpeed = data.wind.speed;
+        const feelsLike = data.main.feels_like;
+        const maxTemp = data.main.temp_max;
+        const minTemp = data.main.temp_min;
+        const sunriseTimeStamp = data.sys.sunrise;
+        const sunriseTime = new Date(
+          sunriseTimeStamp * 1000
+        ).toLocaleTimeString();
+        const sunsetTimeStamp = data.sys.sunset;
+        const sunsetTime = new Date(
+          sunsetTimeStamp * 1000
+        ).toLocaleTimeString();
 
         // Update weather info on the page
         weatherInfo.innerHTML = `
-            <h2>${locationName}</h2>
-            <p><strong>Weather:</strong> ${description}</p>
+            <h2>${locationName} ${countryName}</h2>
+            <p><strong>Cordinates: </strong>Lon:${longitude}, Lat:${latitude}</p>
+            <p><strong>Weather:</strong> ${weatherMain}</p>
+            <p><strong>Description:</strong> ${description}</p>
             <p><strong>Temperature:</strong> ${temperature} °C</p>
+            <p><strong>Feels like:</strong> ${feelsLike} °C</p>
+            <p><strong>Max Temperature:</strong> ${maxTemp} °C</p>
+            <p><strong>Min Temperature:</strong> ${minTemp} °C</p>
             <p><strong>Humidity:</strong> ${humidity} %</p>
             <p><strong>Wind Speed:</strong> ${windSpeed} m/s</p>
+            <p><strong>Sunrise Time:</strong> ${sunriseTime} </p>
+            <p><strong>Sunset Time:</strong> ${sunsetTime} </p>
           `;
       }
     })
